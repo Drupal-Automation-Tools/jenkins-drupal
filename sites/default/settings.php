7eb050dc33b8a6a30cfc2939d1e4a83bee065192
Sean Hanford <shanford@navigationarts.com>
2012-11-27 15:27:20 -0500

    Added hash_salt configuration settings for settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -251,7 +251,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'abc123';
 
 /**
  * Base URL (optional).
